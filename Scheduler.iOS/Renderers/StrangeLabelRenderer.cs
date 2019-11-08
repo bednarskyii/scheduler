844e0af8b32ec81bcd5753ad5f7060ec689b203f
bednarskyi <sv.bednarskyi@gmail.com>
2019-11-08 14:09:43 +0200

    Added custom controll and ios render

--- Scheduler.iOS/Renderers/StrangeLabelRenderer.cs
+++ Scheduler.iOS/Renderers/StrangeLabelRenderer.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using Scheduler.CustomControlls;
+using Scheduler.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(BackgroundLabel), typeof(StrangeLabelRenderer))]
 namespace Scheduler.iOS.Renderers
 {
-    public class StrangeLabelRenderer
+    public class StrangeLabelRenderer : LabelRenderer
     {
-        public StrangeLabelRenderer()
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
         {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.BackgroundColor = UIColor.FromRGB(204, 153, 255);
+            }
         }
     }
 }
---
+++ Scheduler/CustomControlls/BackgroundLabel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Scheduler.CustomControlls
+{
+    public class BackgroundLabel : Label
+    {
+    }
+}
